fd0b4776113870b8ef182ccc1c7a3299a245bcab
Ky2070 <2051052070@ou.edu.vn>
2025-07-10 20:11:31 +0700

    Add DI-Test project

---
+++ DI-Test/MyScopedService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_Test
+{
+    internal class MyScopedService : IMyScopedService
+    {
+        private static int Id = 0;
+        public MyScopedService()
+        {
+            Console.WriteLine($"MyScopedService created with Id: {++Id}");
+        }
+    }
+}
